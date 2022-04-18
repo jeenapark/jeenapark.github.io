4ba3589a7beaf2a6129d72029ee720c2ad7646ea
Jeena Park <jeenapark15@gmail.com>
2022-04-17 21:15:34 -0400

    added circle smiley face when you click the circle

--- index.js
+++ index.js
@@ -5,8 +5,20 @@ ctx.fillStyle = '#0ff';
 ctx.fillRect(10, 10, 25, 25);
 
 const circle = document.getElementById('circle');
-
+function getRandomColor() {
+    return '#f0f';
+}
 function addCircle() {
-    console.log("adding circle");
+    ctx.fillStyle = getRandomColor();
+    ctx.beginPath();
+    ctx.arc(75, 75, 30, 0, Math.PI * 2, true); // Outer circle
+    ctx.fill()
+    ctx.moveTo(95, 75);
+    ctx.arc(75, 75, 20, 0, Math.PI, false);  // Mouth (clockwise)
+    ctx.moveTo(65, 65);
+    ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
+    ctx.moveTo(95, 65);
+    ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
+    ctx.stroke();
 }
 circle.addEventListener('click', addCircle);
